97335fddab7aebeb4dfaf488a1fe7940b4efa8a9
MasanaoAsato <sub.a.cmyspace@gmail.com>
2026-03-05 19:44:35 +0900

    fix:format修正

--- src/domain/value_object/task_id.test.ts
+++ src/domain/value_object/task_id.test.ts
@@ -1,18 +1,17 @@
-import { describe, it, expect } from 'vitest';
-import { TaskId } from './task_id';
-import { ulid } from 'ulid';
-import { ValueObjectError } from '../../utils/errors';
-
+import { describe, it, expect } from "vitest";
+import { TaskId } from "./task_id";
+import { ulid } from "ulid";
+import { ValueObjectError } from "../../utils/errors";
 
 describe("TaskId", () => {
-    it("create TaskId valid", () => {
-        const id = ulid();
-        const taskId = new TaskId(id);
-        expect(taskId.value()).toBe(id);
-    })
+	it("create TaskId valid", () => {
+		const id = ulid();
+		const taskId = new TaskId(id);
+		expect(taskId.value()).toBe(id);
+	});
 
-    it("create TaskId invalid", () => {
-        const id = "test title";
-        expect(() => new TaskId(id)).toThrow(ValueObjectError);
-    })
-})
+	it("create TaskId invalid", () => {
+		const id = "test title";
+		expect(() => new TaskId(id)).toThrow(ValueObjectError);
+	});
+});
